e68b1f2ab2123b325757ffa5277c4b8b5bc3e57b
Rahul <rahwinside@gmail.com>
2021-05-21 11:08:37 +0530

    Better error handling, add pandas

--- main.py
+++ main.py
@@ -1,32 +1,42 @@
 import discord
-from settings import TOKEN
-from mysql_config import cnx
+import pandas as pd
+from settings import TOKEN, mysql_cnx
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
-  print(f"Logged in as {client.user}")
+    print(f"Logged in as {client.user}")
 
 
 @client.event
 async def on_message(message):
-  if message.author == client.user:
-    return
-  
-  if message.content.startswith('!viki'):
-    command = message.content.split(' ', 1)[1]
-    if len(command) == 0:
-      await message.channel.send(f'Hello, {message.author}')
-    if command.startswith('sql'):
-      try:
-        sql = command.split(' ', 1)[1]
-        cursor = cnx.cursor()
-        cursor.execute(sql)
-        rows = cursor.fetchall()
-        await message.channel.send(f"`{str(rows)[0:1998]}`")
-      except Exception as e:
-        await message.channel.send(f"`{e}`")
+    if message.author == client.user:
+        return
+
+    # trim message
+    message.content.lstrip().rstrip()
+    if message.content.startswith('!viki'):
+        try:
+            command = message.content.split(' ', 1)[1]
+        except:
+            print("No command, say hi!")
+            await message.channel.send(f'Hello, {message.author}')
+            return
+
+        if command.startswith('sql'):
+            try:
+                sql = command.split(' ', 1)[1]
+                cursor = mysql_cnx.cursor()
+                cursor.execute(sql)
+                rows = cursor.fetchall()
+                fields = [i[0] for i in cursor.description]
+                out = pd.DataFrame(rows, columns=fields)
+                cursor.close()
+                await message.channel.send(f"`{str(out)[0:1998]}`")
+            except Exception as e:
+                await message.channel.send(f"`{e}`")
 
 
 client.run(TOKEN)
--- settings.py
+++ settings.py
@@ -1,1 +1,9 @@
 TOKEN = "ODQ0OTY3NTkxMzA2ODU0NDIw.YKaHZA.Is1vV_oJCCUO01R7PWrxVRKLtbY"
+
+import mysql.connector
+
+mysql_cnx = mysql.connector.connect(
+    host="vpn.opencloud.pattarai.in",
+    port=3306,
+    user="tux",
+    password="licet@123")
